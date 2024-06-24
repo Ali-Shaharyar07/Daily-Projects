9e8a26e3077b89e7173c70c192a91bdcb9256139
Ali Shaharyar <alishaharyar@pm.me>
2024-06-25 02:30:34 +0500

    Update KisaanKar.ino

--- KIsaanKar/KisaanKar.ino
+++ KIsaanKar/KisaanKar.ino
@@ -2,7 +2,7 @@
 #include <Servo.h>
 SoftwareSerial bt(10, 11);
 Servo s1;
-
+Servo s2;
 #define rb 3
 #define rf 4
 #define lb 7
@@ -19,20 +19,31 @@ void setup() {
   // put your setup code here, to run once:
   bt.begin(9600);
   pinMode(lb, OUTPUT);
+  pinMode(rb, OUTPUT);
+  pinMode(rf, OUTPUT);
+  pinMode(lf, OUTPUT);
   pinMode(relay, OUTPUT);
   Serial.begin(9600);
   digitalWrite(relay, HIGH);
+  s1.attach(12);
+  s1.write(180);
+  s2.attach(13);
+  s2.write(180);
+  digitalWrite(lb, LOW);
+  digitalWrite(rb, LOW);
+  digitalWrite(rf, LOW);
+  digitalWrite(lf, LOW);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  
-     m = analogRead(ms);
-      m = map(m, 260, 600, 100, 0);
+    m = analogRead(ms);
+    m = map(m, 260, 600, 100, 0);
     //Serial.println("m: "+ String(m));
-    bt.println("*m"+ String(m) + "*");
-  
- 
+    if(m>0){
+       bt.println("*m"+ String(m) + "*");
+    }
+    delay(100);
   if(bt.available()){
     data = bt.read();
     Serial.println(data);
@@ -66,6 +77,22 @@ void loop() {
       digitalWrite(relay, HIGH);
       break;
 
+      case 'S':
+      s2.write(0);
+      break;
+
+      case 'D':
+      s2.write(180);
+      break;
+
+       case 'v':
+      s1.write(50);
+      break;
+
+      case 'c':
+      s1.write(180);
+      break;
     }
+    delay(100);
 }
 }
